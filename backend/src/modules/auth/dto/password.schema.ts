2b2cf113ad26d96b046e777ca3f505f6b3ea147f
shikhar0718 <shikhar0718@gmail.com>
2026-08-14 09:05:49 +0530

    not useful commit

--- backend/src/modules/auth/dto/password.schema.ts
+++ backend/src/modules/auth/dto/password.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const passwordValidation = z
   .string()
-  .min(8, "Password must be at least 8 characters long.")
+  .min(6, "Password must be at least 6characters long.")
   .max(128, "Password cannot exceed 128 characters.")
   .refine((password) => /[A-Z]/.test(password), {
     message: "Password must contain at least one uppercase letter.",
